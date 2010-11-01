d7c4731a1fc672107b5c31aa6389cc072b6e892b
Elis Axelsson <elis.axelsson@gmail.com>
2010-11-01 19:30:11 +0100

    This should work, untested init. Will test later :)

--- init.php
+++ init.php
@@ -1,4 +1,9 @@
 <?php
+/***
+ * File: ./init.php
+ * This file is an example of how you initializes the lib, auths the user, get accsess_token, and fetches the user.
+ */
+
 session_start();
 
 require_once('../config.php');
@@ -6,26 +11,19 @@ require_once('../lib.php');
 
 $facebook = new EtuFB($fb);
 
-$fb_atoken = $_SESSION['accsess_token'];
-$fb_code   = $_SESSION['code'];
+$sess['code']   = $_SESSION['code'];
 
-if(strlen($fb_atoken) == 0 && !isset($_GET['code']))
-	$err = '<script>window.top.location = "'.$facebook->getAuthUrl().'";</script>';
+if (strlen($sess['code']) == 0 AND !isset($_GET['code']))
+	die('<script>window.top.location = "'.$facebook->getAuthUrl().'";</script>');
 
-if (strlen($fb_atoken) == 0 && isset($_GET['code'])) {
+if (strlen($sess['code']) != 0 AND isset($_GET['code'])) {
+	$sess['code']     = $_GET['code'];
 	$_SESSION['code'] = $_GET['code'];
-	$fb_code = $_GET['code'];
 	
 	$get_token = $facebook->getAccsessToken($_GET['code']);
 	
-	if(strlen($get_token) != 0) {
-		$_SESSION['accsess_token'] = $get_token;
-		$fb_atoken = $get_token;
-	} else
-		$err = 'Unable to get accsess token, try to reload the page and clear your cookies or something :(';
+	$_SESSION['accsess_token'] = $get_token;
+	$sess['accsess_token']     = $get_token;
 }
 
-if($err)
-	die($err);
-
-$user = $facebook->api('me', $fb_atoken);
+$user = $facebook->api('me', $sess['accsess_token']);
