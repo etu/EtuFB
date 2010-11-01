07b2d9fefdaf74f8df058742ef8c593816371ce7
Elis Axelsson <elis.axelsson@gmail.com>
2010-11-01 18:14:59 +0100

    Added some docs in the header of the file

--- lib.php
+++ lib.php
@@ -1,4 +1,17 @@
 <?php
+/***
+ * This is the acctual lib
+ *
+ ** function getAuthUrl()
+ * Returns the URL you should send the user to, to authenticate and get a code.
+ *
+ ** function getAccsessToken()
+ * Tries to get a accsess token using the code, if it succseed it will return the accsesstoken.
+ * If it fails(usaly becouse tho code is too old) it well reouthenticate the user.
+ *
+ ** function api()
+ * Needs moar work to get it working with all kinds of graph api-calls.
+ */
 
 class EtuFB {
 	var $app_id;
@@ -6,20 +19,18 @@ class EtuFB {
 	var $redirect_url;
 	var $extended_permissions;
 	
-	var $error_reporting;
-	
 	function __construct($app_id, $secret, $redirect_url, $extpermissions = '') {
 		$this->app_id = $app_id;
 		$this->app_secret = $secret;
 		$this->redirect_url = $redirect_url;
 		$this->extended_permissions = $extpermissions;
 		
-		if(ini_get('allow_url_fopen') != 1)
+		if(ini_get('allow_url_fopen') != 1) // Without this permission you will not be able to fetch accsesstoken nor do api-calls
 			die('You have to enable <em>allow_url_fopen</em>, file_get_contents have to be able to accsess URLs to fetch the users accsess_token.');
-		if(ini_get('display_errors') != 0)
+		if(ini_get('display_errors') != 0)  // It's allways a good idea to disable /display_errors/ in a live enviorment, debugmode will be added
 			die('You have to disable <em>display_errors</em>, else it will not work when the accsess_token run out of time.');
 	}
-
+	
 	function getAuthUrl() {
 		$auth_url = 'https://graph.facebook.com/oauth/authorize'.
 			'?client_id='.$this->app_id.
@@ -27,7 +38,7 @@ class EtuFB {
 			'&scope='.$this->extended_permissions;
 		return $auth_url;
 	}
-
+	
 	function getAccsessToken($code) {
 		$accsess_token_url = 'https://graph.facebook.com/oauth/access_token'.
 			'?client_id='.$this->app_id.
