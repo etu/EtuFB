33e09bfcf14a08359a549a8545df649a555a5b3f
Elis Axelsson <elis.axelsson@gmail.com>
2010-11-06 04:54:59 +0100

    Maked it more clear wich file your are watching it my example

--- http/index2.php
+++ http/index2.php
@@ -9,7 +9,7 @@ require_once('../init.php');
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> 
   </head>
   <body>
-	<h1>Signed in</h1>
+	<h1>/index2.php: Signed in</h1>
 	<a href="index.php?code=<?=$facebook->code?>">Link to index.php</a>
   </body>
 </html>
