64ea761d16e8c041bf4db596c81aaba79280d5df
Elis Axelsson <elis.axelsson@gmail.com>
2011-01-16 12:32:15 +0100

    Changed from app to api in the coments.

--- config.sample.php
+++ config.sample.php
@@ -4,23 +4,23 @@
  * Configfile
  *
  ** $fb['api_id']
- * Your app-id, the one you gets from facebook.
+ * Your api-id, the one you gets from facebook.
  *
  ** $fb['secret']
- * Your app-secret, you get this from facebook too ;)
+ * Your api-secret, you get this from facebook too ;)
  *
  ** $fb['url']
- * The canvaspage for your application
+ * The canvaspage for your application.
  *
  ** $fb['redirect']
- * The url where you should get redirected after you get authentication
+ * The url where you should get redirected after you get authentication.
  *
  ** $fb['eperms']
  * Extended permissions you want to require from the user, for example: publish_stream,email,sms
  */
 
-$fb['api_id']   = '<your appid>';
-$fb['secret']   = '<your appsecret>';
+$fb['api_id']   = '<your api-id>';
+$fb['secret']   = '<your api-secret>';
 $fb['url']      = 'http://apps.facebook.com/<your appcanvas here>/';
 $fb['redirect'] = $fb['url'].'?page=oauth_redirect';
 $fb['eperms']   = '';
