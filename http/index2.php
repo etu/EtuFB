98adf492afceb96e460544dab1de686afb6cdc2d
Elis Axelsson <elis.axelsson@gmail.com>
2011-09-13 14:00:22 +0200

    symmetry

--- http/index2.php
+++ http/index2.php
@@ -6,10 +6,11 @@ require_once('../init.php');
 	xmlns:fb="http://www.facebook.com/2008/fbml">
   <head>
 	<title>FBAPP</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> 
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
   </head>
   <body>
 	<h1>/http/index2.php: Authed</h1>
-	<a href="index.php?code=<?=$facebook->code?>">Link to index.php</a>
+	<a href="/index.php?code=<?=$facebook->code?>">Link to index2.php</a>
+	<hr />
   </body>
 </html>
